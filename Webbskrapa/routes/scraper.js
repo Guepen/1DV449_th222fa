c4c47b6f389735392b60db73a946bc31c43b5e7c
th222fa <th222fa@student.lnu.se>
2014-11-06 00:31:46 +0100

    kan nu hämta ut alla kurser

--- Webbskrapa/routes/scraper.js
+++ Webbskrapa/routes/scraper.js
@@ -7,42 +7,54 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-var coursename, courseUrl, courseCode;
-var json = {coursename : "", courseUrl : "", courseCode : ""};
-var url = "http://coursepress.lnu.se/kurser";
-var $;
+
+
 
 /* GET users listing. */
 app.get('/', function(req, res) {
+    var url = "http://coursepress.lnu.se/kurser/?bpage=1";
 
-    var nextPage = $('#pag-top .next');
-    nextPage = nextPage.attr('href');
-    nextPage = nextPage.replace('/kurser', '');
-    nextPage = url + nextPage;
-    doRequest(nextPage);
+    request(url, function (error, resp, body) {
+        var lastPage;
 
-    res.send();
+        var $ = cheerio.load(body);
+
+        //not a secure solution. The webowner can change the structure of the DOM-elements
+        lastPage = $('#blog-dir-pag-top :nth-last-child(2)').attr('href');
+
+        //using slice to get the last character containing the last pagenumber
+        lastPage = lastPage.slice(-1);
+
+        scraper(lastPage);
+    });
+
+
 });
 
-var doRequest = function(url){
+var scraper = function(lastPage) {
+    var coursename, courseUrl, courseCode, $;
+    var json = {coursename: "", courseUrl: "", courseCode: ""};
+
+    //loop through every page with courses
+    for (var i = 1; i <= lastPage; i++) {
+
+        var url = "http://coursepress.lnu.se/kurser/?bpage=" + i;
+        request(url, function (error, response, body) {
+            if (!error) {
+                $ = cheerio.load(body);
+
+                $('.item-title a').filter(function () {
+                    var data = $(this);
+                    json.coursename = data.html();
+                    json.courseUrl = data.attr('href');
+                    console.log(json.coursename);
+                });
 
 
-    request(url, function(error, response, html){
-        if(!error){
-            $ = cheerio.load(html);
+            }
 
-            $('.item-title a').filter(function(){
-                var data = $(this);
-                json.coursename = data.html();
-                json.courseUrl = data.attr('href');
-                console.log(json.coursename);
-
-            })
-
-
-        }
-
-    });
+        })
+    }
 };
 
 module.exports = app;
