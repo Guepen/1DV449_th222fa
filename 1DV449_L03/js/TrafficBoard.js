90ffeea64a677171fe3bfef659803184be24f607
th222fa <th222fa@student.lnu.se>
2014-12-01 16:27:30 +0100

    sparat JSON-responset i textfil för att inte göra onödiga requests under utvecklinngstiden.

--- 1DV449_L03/js/TrafficBoard.js
+++ 1DV449_L03/js/TrafficBoard.js
@@ -1,3 +1,37 @@
 /**
  * Created by Tobias on 2014-12-01.
  */
+var TrafficBoard = {
+    init: function(){
+        TrafficBoard.getTrafficPosts();
+    },
+
+    getTrafficPosts: function(){
+        var request = $.ajax({
+            "type": "GET",
+            "url": "ajaxCalls/TrafficAlertsHandler.php",
+            "dataType": "json",
+            "data": {"mode": "getAlerts"}
+        });
+
+        request.success(function(data){
+            var jsonData = $.parseJSON(data)
+            console.log(jsonData["messages"]);
+        });
+
+        request.fail(function( xhr, textStatus){
+            console.log(xhr, textStatus);
+        })
+
+
+    },
+
+    renderTrafficAlert: function(trafficAlert){
+        console.log(trafficAlert);
+    }
+};
+
+
+
+
+window.onload = TrafficBoard.init;
--- 1DV449_L03/pages/EchoHTML.php
+++ 1DV449_L03/pages/EchoHTML.php
@@ -13,6 +13,7 @@ class EchoHTML {
 				<!DOCTYPE html>
 				<html>
 				<head>
+				 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 				<!-- Latest compiled and minified CSS -->
                 <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
 
@@ -23,13 +24,35 @@ class EchoHTML {
 				<title>TrafikInfo</title>
 				</head>
 				<body>
-				   <div id='Container'>
-                        <h1 class='center'>Trafikinformation</h1>
-					$body
-                   </div>
-                     <script src='//code.jquery.com/jquery-1.11.0.min.js'></script>
+				   <div class='container'>
+				     <nav class='navbar navbar-inverse'>
+                        <div class='container-fluid'>
+                            <div class='navbar-header'>
+                                <button type='button' class='navbar-toggle' data-toggle='collapse'
+                                        data-target='#bs-example-navbar-collapse-9'>
+                                    <span class='sr-only'>Toggle navigation</span>
+                                    <span class='icon-bar'></span>
+                                    <span class='icon-bar'></span>
+                                    <span class='icon-bar'></span>
+                                </button>
+                            </div>
+                            <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-9'>
+                                <ul class='nav navbar-nav'>
+                                    <li><a href='#'>Hem</a></li>
+                                    <li><a href='#'>Om Sidan</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
+                    <h1>Trafikinformation</h1>
+                    $body
+
+				   </div>
+                    <script src='//code.jquery.com/jquery-1.11.0.min.js'></script>
                      <!-- Latest compiled and minified JavaScript -->
                     <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
+                    <script src='js/TrafficBoard.js'></script>
 				</body>
 				</html>";
     }
