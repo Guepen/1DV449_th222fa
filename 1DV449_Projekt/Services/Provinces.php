e42145be6a1278174b3d8ed3a228b22669e5879a
th222fa <th222fa@student.lnu.se>
2014-12-12 15:10:48 +0100

    hämtning av län och antal jobb i länet klart

---
+++ 1DV449_Projekt/Services/Provinces.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tobias
+ * Date: 2014-12-11
+ * Time: 16:24
+ */
+require_once('../../vendor/rmccue/requests/library/Requests.php');
+Requests::register_autoloader();
+
+new Provinces();
+
+class Provinces {
+    public function __construct(){
+        $this->getProvinces();
+    }
+
+    private function getProvinces(){
+        if (!file_exists("Provinces.json")) {
+            $response = Requests::get(
+                "http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/lan",
+                array(
+                    'Accept' => 'application/json',
+                    'Accept-Language' => 'sv'
+                )
+            );
+
+            if ($response->status_code == 200) {
+                file_put_contents("Provinces.json", $response->body);
+            }
+
+            $this->sendResponse($response->body);
+        } else{
+            $this->sendResponse(file_get_contents("Provinces.json"));
+        }
+    }
+
+    private function sendResponse($response){
+        echo $response;
+    }
+} 
--- 1DV449_Projekt/js/JobBoard.js
+++ 1DV449_Projekt/js/JobBoard.js
@@ -5,6 +5,47 @@
 var JobBoard = {
 
     init: function(){
+        $("#location-search").click(function(){
+            JobBoard.getProvinces();
+        })
+
+
+    },
+
+    getProvinces: function(){
+
+        $.ajax({
+            "type": "GET",
+            "url": "Services/Provinces.php",
+            "dataType": "json",
+            success: function(data){
+                if(data.soklista.sokdata.length > 0){
+                    JobBoard.renderProvinces(data.soklista.sokdata);
+                }
+            },
+
+            error: function(xhr, text){
+                alert(text);
+            }
+        });
+    },
+
+    renderProvinces: function(provinces){
+        var parent = $(".jumbotron");
+        var ul = $("<ul></ul>");
+        ul.appendTo(parent);
+        provinces.forEach(function(province){
+           var li =$("<li></li>");
+            var provinceLink = $("<a id=" + province.id +"  href='#'>" + province.namn +
+            "(" + province.antal_platsannonser + ")" + "</a>");
+            li.appendTo(ul);
+            provinceLink.appendTo(li);
+
+            provinceLink.click(function(){
+                console.log(event.target.id);
+            })
+        });
+
 
     }
 
